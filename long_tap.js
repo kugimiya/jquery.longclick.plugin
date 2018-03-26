db1888cdfb09219ffe29ac39766cee78dbb6dfe1
Andrew Goncharov <bp.001@yandex.ru>
2018-03-26 10:58:25 +0400

    Add sending event onject to onCallEnd function.

--- long_tap.js
+++ long_tap.js
@@ -8,124 +8,124 @@
 
 (function ($) {
 
-  var longTapInstanceLogic = function (options) {
-    return function (key, self) {
+    var longTapInstanceLogic = function (options) {
+        return function (key, self) {
 
-      var timeout      = options.timeout || 500,
-          onStartDelay = options.onStartDelay || 0,
-          onEndDelay   = options.onEndDelay || 50,
-          mouseEvents  = options.mouseEvents || false,
-          touchEvents  = options.touchEvents || false,
-          $self        = $(self),
-          clickState   = false,
-          commonState  = false,
-          dummyClick   = false,
-          eventState   = '',
-          timer,
-          onStartTimer;
-      
-      var timeoutCallback = function (event) {
-        if (!commonState) {
-          if (options.onSuccess) {
-            options.onSuccess(event, $self);
-          }
-        } else {
-          if (options.onReject) {
-            options.onReject(event, $self);
-          }
+            var timeout = options.timeout || 500,
+                onStartDelay = options.onStartDelay || 0,
+                onEndDelay = options.onEndDelay || 50,
+                mouseEvents = options.mouseEvents || false,
+                touchEvents = options.touchEvents || false,
+                $self = $(self),
+                clickState = false,
+                commonState = false,
+                dummyClick = false,
+                eventState = '',
+                timer,
+                onStartTimer;
+
+            var timeoutCallback = function (event) {
+                if (!commonState) {
+                    if (options.onSuccess) {
+                        options.onSuccess(event, $self);
+                    }
+                } else {
+                    if (options.onReject) {
+                        options.onReject(event, $self);
+                    }
+                }
+
+                clickState = (!clickState);
+                commonState = (!commonState);
+
+                callOnEnd(event);
+            };
+
+            var onStartDelayCallback = function (event) {
+                if (dummyClick) {
+                    callOnEnd(event);
+                    return;
+                }
+
+                if (options.onStart) {
+                    options.onStart(event, $self);
+                }
+
+                if (clickState) {
+                    clickState = (!clickState);
+                }
+
+                eventState = 'processing';
+                timer = setTimeout(timeoutCallback, timeout, event);
+                clearTimeout(onStartTimer);
+            };
+
+            var startEventHandler = function (event) {
+                eventState = 'start';
+                onStartTimer = setTimeout(onStartDelayCallback, onStartDelay, event);
+            };
+
+            var endEventHandlerLogic = function (event) {
+                var canIEndThis = (
+                    (!clickState) &&
+                    (
+                        (typeof (onStartTimer) == typeof (undefined)) ||
+                        (typeof (timer) != typeof (undefined))
+                    )
+                );
+
+                if (canIEndThis) {
+                    callOnEnd(event);
+                }
+
+                clearTimeout(timer);
+            }
+
+            var endEventHandler = function (event) {
+                setTimeout(endEventHandlerLogic, onEndDelay, event);
+            };
+
+            var registerClick = function (event) {
+                if (eventState == 'start') {
+                    dummyClick = true;
+                }
+            }
+
+            var callOnEnd = function (event) {
+                if (options.onEnd && (dummyClick != true)) {
+                    options.onEnd(event, $self, commonState);
+                }
+
+                dummyClick = false;
+                eventState = 'ended';
+            }
+
+            if (mouseEvents && (!touchEvents)) {
+                $self.on('mousedown', startEventHandler);
+                $self.on('mouseup', endEventHandler);
+            }
+
+            if (touchEvents && (!mouseEvents)) {
+                $self.on('touchstart', startEventHandler);
+                $self.on('touchend', endEventHandler);
+            }
+
+            $self.on('click', registerClick);
         }
-
-        clickState  = (!clickState);
-        commonState = (!commonState);
-
-        callOnEnd();
-      };
-
-      var onStartDelayCallback = function (event) {
-        if (dummyClick) {
-          callOnEnd();
-          return;
-        }
-
-        if (options.onStart) {
-          options.onStart(event, $self);
-        }
-
-        if (clickState) {
-          clickState = (!clickState);
-        }
-
-        eventState = 'processing';
-        timer      = setTimeout(timeoutCallback, timeout, event);
-        clearTimeout(onStartTimer);
-      };
-
-      var startEventHandler = function (event) {
-        eventState   = 'start';
-        onStartTimer = setTimeout(onStartDelayCallback, onStartDelay, event);
-      };
-
-      var endEventHandlerLogic = function (event) {
-        var canIEndThis = (
-          (!clickState) && 
-          (
-            (typeof(onStartTimer) == typeof(undefined)) ||
-            (typeof(timer)        != typeof(undefined))
-          )
-        );
-
-        if (canIEndThis) {
-          callOnEnd();
-        }
-
-        clearTimeout(timer);
-      }
-
-      var endEventHandler = function (event) {
-        setTimeout(endEventHandlerLogic, onEndDelay, event);
-      };
-
-      var registerClick = function (event) {
-        if (eventState == 'start') {
-          dummyClick = true;
-        }
-      }
-
-      var callOnEnd = function () {
-        if (options.onEnd && (dummyClick != true)) {
-          options.onEnd(event, $self, commonState);
-        }
-
-        dummyClick = false;
-        eventState = 'ended';
-      }
-
-      if (mouseEvents && (!touchEvents)) {
-        $self.on('mousedown', startEventHandler);
-        $self.on('mouseup', endEventHandler);
-      }
-
-      if (touchEvents && (!mouseEvents)) {
-        $self.on('touchstart', startEventHandler);
-        $self.on('touchend', endEventHandler);
-      }
-
-      $self.on('click', registerClick);
     }
-  }
 
-  $.fn.longTap = function (options = {
-    onStart,
-    onSuccess,
-    onReject,
-    onEnd,
-    onStartDelay,
-    onEndDelay,
-    timeout,
-    mouseEvents,
-    touchEvents
-  }) {
-    return this.each(longTapInstanceLogic(options));
-  }
+    $.fn.longTap = function (options = {
+        onStart,
+        onSuccess,
+        onReject,
+        onEnd,
+        onStartDelay,
+        onEndDelay,
+        timeout,
+        mouseEvents,
+        touchEvents
+    }) {
+        return this.each(longTapInstanceLogic(options));
+    }
 
 })(jQuery);
